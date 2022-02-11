a17c66b7974b71f1c73943e7c6c7adbaa252d1fb
Prasanth Kondapi <kvskrishnaprasanth@gmail.com>
2022-02-11 13:10:43 +0100

    adding optimization change of index

--- lab1/multiply/multiply.c
+++ lab1/multiply/multiply.c
@@ -43,11 +43,11 @@ matmul_opt()
          * here. It should calculate mat_c := mat_a * mat_b. See
          * matmul_ref() for a reference solution.
          */
-        int i, j, k;
+        int k, i, j;
 
         for (i = 0; i < SIZE; i++) {
-                for (j = 0; j < SIZE; j++) {
-                        for (k = 0; k < SIZE; k++) {
+                for (k = 0; k < SIZE; k++) {
+                        for (j = 0; j < SIZE; j++) {
                                 mat_ref[i][j] += mat_a[i][k] * mat_b[k][j];
                         }
                 }
